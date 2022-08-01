1b6ea30d98cd11685af6089eaa4130daee34af10
Akifumi Nakamura <a-nakamura@studio-spoon.co.jp>
2022-08-01 14:40:22 +0900

    make index

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,11 @@ export default defineConfig(({ mode }) => {
 		build: {
 			rollupOptions: {
 				input: {
-					home01: path.resolve(__dirname, './src/01/index.html'),
-					home02: path.resolve(__dirname, './src/02/index.html'),
-					home03: path.resolve(__dirname, './src/03/index.html'),
-					home04: path.resolve(__dirname, './src/04/index.html')
+					home: path.resolve(__dirname, './src/index.html'),
+					scene01: path.resolve(__dirname, './src/01/index.html'),
+					scene02: path.resolve(__dirname, './src/02/index.html'),
+					scene03: path.resolve(__dirname, './src/03/index.html'),
+					scene04: path.resolve(__dirname, './src/04/index.html')
 				}
 			},
 			outDir: '../dist'
